ffa6109e6e7b2e8645dd8e043bfb049bbb5d932e
Sagar Pandit <kumarsatyaki7@gmail.com>
2020-06-30 00:08:41 +0530

    Changes for Entity Framwork code forst

---
+++ EShop/EShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using EShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
