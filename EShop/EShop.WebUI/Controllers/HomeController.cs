56da2f7935df19b652506a6955b8c40d6bb4429e
Sagar Pandit <kumarsatyaki7@gmail.com>
2020-07-15 23:58:35 +0530

    product listing home page

--- EShop/EShop.WebUI/Controllers/HomeController.cs
+++ EShop/EShop.WebUI/Controllers/HomeController.cs
@@ -1,16 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using EShop.Core.Contracts;
 using System.Web.Mvc;
+using Antlr.Runtime.Misc;
+using EShop.Core.Models;
 
 namespace EShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRepository<Product> _productContext;
+        private readonly IRepository<ProductCategory> _productCategoryContext;
+
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            this._productContext = productContext;
+            this._productCategoryContext = productCategoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = _productContext.Collection().ToList(); 
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = _productContext.Find(Id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
